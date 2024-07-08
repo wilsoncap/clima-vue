dbb872d6cb4f6d335fed3f7179d0bd2fcade7266
Wilson Capacho <wiston.98@hotmail.com>
2024-07-07 19:33:54 -0500

    validacion formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,28 +1,44 @@
 <script setup>
-
-    import { reactive } from 'vue';
-
+    import { reactive, ref } from 'vue';
+    import Alerta from './Alerta.vue';
     const busqueda = reactive({
         ciudad: '',
         pais: ''
     })
 
+    const error = ref('')
+
     const paises = [
-    { codigo: 'US', nombre: 'Estados Unidos' },
-    { codigo: 'MX', nombre: 'México' },
-    { codigo: 'AR', nombre: 'Argentina' },
-    { codigo: 'CO', nombre: 'Colombia' },
-    { codigo: 'CR', nombre: 'Costa Rica' },
-    { codigo: 'ES', nombre: 'España' },
-    { codigo: 'PE', nombre: 'Perú' }
-]
+        { codigo: 'US', nombre: 'Estados Unidos' },
+        { codigo: 'MX', nombre: 'México' },
+        { codigo: 'AR', nombre: 'Argentina' },
+        { codigo: 'CO', nombre: 'Colombia' },
+        { codigo: 'CR', nombre: 'Costa Rica' },
+        { codigo: 'ES', nombre: 'España' },
+        { codigo: 'PE', nombre: 'Perú' }
+    ]
+
+    const consultarClima = () => {
+        if (Object.values(busqueda).includes('')) {
+            error.value = 'Todos los campos son obligatorios'
+            setTimeout(() =>{
+                error.value = ''
+            }, 3000)
+            return
+        }
+
+        error.value = ''
+    }
 </script>
 
 
 <template>
       <form 
   class="formulario"
+  @submit.prevent="consultarClima"
   >
+    <Alerta v-if="error">{{ error }}</Alerta>
+
     <div class="campo">
       <label for="ciudad">Ciudad</label>
       <input 
