6de134bde1c0745688568d6039b23db818b43f35
Wilson Capacho <wiston.98@hotmail.com>
2024-07-07 19:11:03 -0500

    formulario principal

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import Formulario from './components/Formulario.vue';
+</script>
+
+<template>
+<h1 class="titulo">Buscador Clima</h1>
+
+<div class="contenedor buscador-clima">
+  <Formulario />  
+</div>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,41 @@
+<script setup>
+     const paises = [
+    { codigo: 'US', nombre: 'Estados Unidos' },
+    { codigo: 'MX', nombre: 'México' },
+    { codigo: 'AR', nombre: 'Argentina' },
+    { codigo: 'CO', nombre: 'Colombia' },
+    { codigo: 'CR', nombre: 'Costa Rica' },
+    { codigo: 'ES', nombre: 'España' },
+    { codigo: 'PE', nombre: 'Perú' }
+]
+</script>
+
+
+<template>
+      <form 
+  class="formulario"
+  >
+    <div class="campo">
+      <label for="ciudad">Ciudad</label>
+      <input 
+            type="text"
+            id="ciudad"
+            placeholder="ciudad"
+        >
+    </div>
+
+    <div class="campo">
+      <label for="pais">Ciudad</label>
+      <select name="" id="pais">
+          <option value="">-- Seleccione un pais --</option>
+          <option v-for="pais in paises" :value="pais.codigo" :key="pais.codigo">{{ pais.nombre }}</option>
+      </select>
+    </div>
+
+    <input type="submit" value="Consultar Clima">
+  </form>
+</template>
+
+
+<style scoped>
+</style>
