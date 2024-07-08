32c90634265f4caea3ad5b2e1ea844676be5f87a
Wilson Capacho <wiston.98@hotmail.com>
2024-07-07 20:13:09 -0500

    primer llamado a la api

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -4,10 +4,12 @@ export default function useClima() {
 
     const obtenerClima = ({ciudad, pais}) => {
       // Importando el Api kery
-        const key  = import.meta.env.VITE_API_KEY
+        const key = import.meta.env.VITE_API_KEY
 
       // Obtener la lat, long
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
 
+        console.log(url);
 
       // Obtener el clima
     }
